e4cad2544bed467970927003efd8b2630c4f244d
Sean Henry <hello@seanhenry.codes>
2017-01-05 13:07:48 +0000

    Hooks up components in app delegate

--- JohnLewis/Main/AppDelegate.swift
+++ JohnLewis/Main/AppDelegate.swift
@@ -5,6 +5,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let productListViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ProductListCollectionViewController
+        let request = JLRequest(baseURL: URL(string: "https://api.johnlewis.com/v1")!)
+        let presenter = ProductListViewPresenter(productListFetcher: NetworkProductListFetcher(request: request), view: productListViewController)
+        productListViewController.eventHandler = presenter
+        window.rootViewController = productListViewController
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 }
